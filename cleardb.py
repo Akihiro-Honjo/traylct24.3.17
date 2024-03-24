51293d82d50142a46542fbcf177ffdd6d51ca405
Akihiro-Honjo <akhr0711.h.a@gmail.com>
2024-03-24 23:15:49 +0900

    update

--- cleardb.py
+++ cleardb.py
@@ -1,20 +1,20 @@
 import mysql.connector
-from PIL import Image
-import io
-
+from urllib.parse import urlparse
+import os
 
 # 画像ファイルをバイナリデータに変換する関数
-# def convert_image_to_binary(image_path):
-#     with open(image_path, 'rb') as file:
-#         binary_data = file.read()
-#     return binary_data
+def convert_image_to_binary(image_path):
+    with open(image_path, 'rb') as file:
+        binary_data = file.read()
+    return binary_data
 
-# MySQLに接続
+# Heroku の環境変数からデータベース接続情報を取得
+url = urlparse(os.getenv('CLEARDB_DATABASE_URL'))
 conn = mysql.connector.connect(
-    host="us-cluster-east-01.k8s.cleardb.net",
-    user="b6ebe5836a9814",
-    password="9c68da67",
-    database="heroku_5d81e4bbe09030e"
+    host=url.hostname,
+    user=url.username,  # 修正: 'username' から 'user' へ
+    password=url.password,
+    database=url.path[1:]  # 先頭の '/' を除去
 )
 
 # カーソルを取得
@@ -32,23 +32,10 @@ CREATE TABLE IF NOT EXISTS tray_product (
     image BLOB    
 )
 """
-# def insert_image_data(product, category, maker, size, features, image_path):
-#     binary_data = convert_image_to_binary(image_path)
-#     insert_query = """
-#     INSERT INTO tray_product (product, category, maker, size, features, image)
-#     VALUES (%s, %s, %s, %s, %s, %s)
-#     """
-#     cursor.execute(insert_query, (product, category, maker, size, features, binary_data))
-#     conn.commit()
+cursor.execute(create_table_query)  # この行を追加
 
-# # 画像データを含むデータを挿入
-# image_path = r'c:\Users\akhr0\OneDrive\デスクトップ\トレー写真\MFPかぐら丼17-17.jpg' 
-# # 挿入するデータをセットアップ
-# tray_data = ("MFPかぐら丼17-17", "丼", "エフピコ", "170×170×44", "丼ぶり ごはん160g 四角", image_path)
-
-# # データ挿入関数を呼び出し
-# insert_image_data(*tray_data)
-
+# 変更をコミット
+conn.commit()  # テーブル作成は変更を伴うため、コミットが必要
 
 # 接続を閉じる
 cursor.close()
